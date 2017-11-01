5ef981802188c7ecb18c9f00839f14531b2b3656
nakajmg <nakajima.jmg@gmail.com>
2017-11-01 20:07:47 +0900

    add fixture

---
+++ fixture.js
@@ -0,0 +1,68 @@
+const Slack = require('slack-node')
+const request = require('axios')
+const URI = process.env.ENV_SLACK_HOOK
+const slack = new Slack()
+slack.setWebhook(URI)
+
+const employees = [
+  'kyosuke',
+  'soto',
+  'nakajmg',
+  'tacamy',
+  '5509',
+  'armorik83',
+  'cancer',
+  'cyokodog',
+  'geckotang',
+  'leader22',
+  'myakura',
+  'obara',
+  'oosugi',
+  'takazudo',
+  'tomof',
+  'y-maru',
+  'ykhs',
+  'yomotsu'
+]
+
+const token = process.env.ENV_SLACK_TOKEN
+const options = {
+  url: 'https://slack.com/api/users.list',
+  method: 'POST',
+  params: {
+    token: token
+  },
+}
+
+request(options)
+  .then(res => res.data)
+  .then(body => {
+    const status = body.members.reduce(function (ret, member) {
+      if (employees.includes(member.name)) {
+        ret[member.name] = {
+          emoji: member.profile.status_emoji,
+          text: member.profile.status_text,
+        }
+      }
+      return ret
+    }, {})
+
+    const mongoose = require('mongoose')
+    mongoose.connect(process.env.MONGODB_URI)
+
+    const StatusSchema = new mongoose.Schema({
+      status: Object
+    })
+    mongoose.model('Status', StatusSchema)
+    const Status = mongoose.model('Status')
+
+    Status.remove({}, (err) => {
+      if (err) throw err
+      const _status = new Status()
+      _status.status = status
+      _status.save((err) => {
+        if (err) throw err
+        mongoose.connection.close()
+      })
+    })
+  })
